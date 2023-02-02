d5ee244c939b47046e48ab073d4f87a57e91f1b0
yancy47 <yancym47@gmail.com>
2023-02-02 11:57:33 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+alert()
+
+// create players one will be person the other is computer 
+//create classes here 
+
+//create ball 
+// create paddles 
+
+// click start button 
+// add click event 
+
+// ball is fired 
+// does ball collide with paddle or wall 
+// add if/else statement here 
+// if ball collides with paddle bounce the ball until it hits the oppessing team's wall or your wall
+// if ball
+
+
